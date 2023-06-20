2697e7a822721d9064a4e811b19e8835b1c3058e
ddeoyoung <ksysnake3690@gmail.com>
2023-06-20 15:12:46 +0900

    엔진 기능 추가 : YSort

--- GameEngineCore/GameEngineCamera.cpp
+++ GameEngineCore/GameEngineCamera.cpp
@@ -10,6 +10,11 @@ GameEngineCamera::~GameEngineCamera()
 {
 }
 
+bool YSortFunction(GameEngineRenderer* _Left, GameEngineRenderer* _Right)
+{
+	return _Left->GetActorYPivot() < _Right->GetActorYPivot();
+}
+
 void GameEngineCamera::Render(float _Delta)
 {
 	//for (const std::pair<int, std::list<GameEngineRenderer*>>& Pair : Renderers)
@@ -26,6 +31,15 @@ void GameEngineCamera::Render(float _Delta)
 		std::list<GameEngineRenderer*>::iterator RenderStartIter = List.begin();
 		std::list<GameEngineRenderer*>::iterator RenderEndIter = List.end();
 
+		if (true == GetYSort(GroupStartIter->first))
+		{
+			// 포인터의 값으로 sort를 합니다
+			// 내부에 있는 값을 비교합니다.
+			// 그냥 하면 포인터의 크기로 합니다.
+			// 포인터는 뭐다? => 8바이트 정수
+			// List.sort();
+			List.sort(YSortFunction);
+		}
 
 		for (; RenderStartIter != RenderEndIter; ++RenderStartIter)
 		{
