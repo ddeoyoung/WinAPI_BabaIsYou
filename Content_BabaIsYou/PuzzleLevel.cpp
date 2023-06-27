eb6c42895be11fc92482e0fb22abad3f5afb723f
ddeoyoung <ksysnake3690@gmail.com>
2023-06-27 14:36:28 +0900

    PuzzleLevel : SetName 수정

--- Content_BabaIsYou/PuzzleLevel.cpp
+++ Content_BabaIsYou/PuzzleLevel.cpp
@@ -75,6 +75,7 @@ void PuzzleLevel::Start()
 			if (MapTexture->GetColor(RGB(0, 0, 0), { fx, fy }) == RGB(255, 0, 0))
 			{ 
 				TileRenderer = TileGrid->SetTile(x, y, 290, BackGridPos);
+				TileRenderer->SetName("WALL");
 				TileRenderer->CreateAnimationToFrame("WALL", "Actor.bmp", { 290, 314, 338 }, 0.2f, true);
 				TileRenderer->ChangeAnimation("WALL");
 			}
@@ -83,6 +84,7 @@ void PuzzleLevel::Start()
 			else if (MapTexture->GetColor(RGB(0, 0, 0), { fx, fy }) == RGB(0, 180, 0))
 			{
 				TileRenderer = TileGrid->SetTile(x, y, 506, BackGridPos);
+				TileRenderer->SetName("GRASS");
 				TileRenderer->CreateAnimationToFrame("GRASS", "Actor.bmp", { 506, 530, 554 }, 0.2f, true);
 				TileRenderer->ChangeAnimation("GRASS");
 			}
@@ -91,6 +93,7 @@ void PuzzleLevel::Start()
 			else if (MapTexture->GetColor(RGB(0, 0, 0), { fx, fy }) == RGB(0, 0, 0))
 			{
 				TileRenderer = TileGrid->SetTile(x, y, 2, BackGridPos);
+				TileRenderer->SetName("BABA");
 				TileRenderer->CreateAnimationToFrame("Baba_Left", "Actor.bmp", { 10, 34, 58 }, 0.2f, true);
 				TileRenderer->CreateAnimationToFrame("Baba_Right", "Actor.bmp", { 2, 26, 50 }, 0.2f, true);
 				TileRenderer->CreateAnimationToFrame("Baba_Up", "Actor.bmp", { 6, 30, 54 }, 0.2f, true);
@@ -119,6 +122,7 @@ void PuzzleLevel::Start()
 			else if (MapTexture->GetColor(RGB(0, 0, 0), { fx, fy }) == RGB(0, 0, 100))
 			{
 				TileRenderer = TileGrid->SetTile(x, y, 737, BackGridPos);
+				TileRenderer->SetName("BLOCK");
 				TileRenderer->CreateAnimationToFrame("BLOCK", "Actor.bmp", { 737, 761, 785 }, 0.2f, true);
 				TileRenderer->ChangeAnimation("BLOCK");
 			}
@@ -127,6 +131,7 @@ void PuzzleLevel::Start()
 			else if (MapTexture->GetColor(RGB(0, 0, 0), { fx, fy }) == RGB(255, 255, 0))
 			{
 				TileRenderer = TileGrid->SetTile(x, y, 728, BackGridPos);
+				TileRenderer->SetName("FLAG");
 				TileRenderer->CreateAnimationToFrame("FLAG", "Actor.bmp", { 728, 752, 776 }, 0.2f, true);
 				TileRenderer->ChangeAnimation("FLAG");
 			}
@@ -135,6 +140,7 @@ void PuzzleLevel::Start()
 			else if (MapTexture->GetColor(RGB(0, 0, 0), { fx, fy }) == RGB(50, 50, 50))
 			{
 				TileRenderer = TileGrid->SetTile(x, y, 288, BackGridPos);
+				TileRenderer->SetName("WALL_TEXT");
 				TileRenderer->CreateAnimationToFrame("WALL_TEXT", "Actor.bmp", { 288, 312, 336 }, 0.2f, true);
 				TileRenderer->ChangeAnimation("WALL_TEXT");
 			}
@@ -143,6 +149,7 @@ void PuzzleLevel::Start()
 			else if (MapTexture->GetColor(RGB(0, 0, 0), { fx, fy }) == RGB(200, 200, 200))
 			{
 				TileRenderer = TileGrid->SetTile(x, y, 792, BackGridPos);
+				TileRenderer->SetName("IS_TEXT");
 				TileRenderer->CreateAnimationToFrame("IS_TEXT", "Actor.bmp", { 792, 816, 840 }, 0.2f, true);
 				TileRenderer->ChangeAnimation("IS_TEXT");
 			}
@@ -151,6 +158,7 @@ void PuzzleLevel::Start()
 			else if (MapTexture->GetColor(RGB(0, 0, 0), { fx, fy }) == RGB(255, 200, 0))
 			{
 				TileRenderer = TileGrid->SetTile(x, y, 726, BackGridPos);
+				TileRenderer->SetName("FLAG_TEXT");
 				//TileRenderer->CreateAnimationToFrame("FLAG_TEXT", "Actor.bmp", { 726, 750, 774 }, 0.2f, true);
 				TileRenderer->CreateAnimationToFrame("FLAG_TEXT", "Actor.bmp", { 727, 751, 775 }, 0.2f, true);
 				TileRenderer->ChangeAnimation("FLAG_TEXT");
@@ -160,6 +168,7 @@ void PuzzleLevel::Start()
 			else if (MapTexture->GetColor(RGB(0, 0, 0), { fx, fy }) == RGB(255, 100, 100))
 			{
 				TileRenderer = TileGrid->SetTile(x, y, 0, BackGridPos);
+				TileRenderer->SetName("BABA_TEXT");
 				TileRenderer->CreateAnimationToFrame("BABA_TEXT", "Actor.bmp", { 0, 24, 48 }, 0.2f, true);
 				//TileRenderer->CreateAnimationToFrame("BABA_TEXT", "Actor.bmp", { 1, 25, 49 }, 0.2f, true);
 				TileRenderer->ChangeAnimation("BABA_TEXT");
@@ -169,6 +178,7 @@ void PuzzleLevel::Start()
 			else if (MapTexture->GetColor(RGB(0, 0, 0), { fx, fy }) == RGB(100, 50, 50))
 			{
 				TileRenderer = TileGrid->SetTile(x, y, 864, BackGridPos);
+				TileRenderer->SetName("YOU_TEXT");
 				TileRenderer->CreateAnimationToFrame("YOU_TEXT", "Actor.bmp", { 864, 888, 912 }, 0.2f, true);
 				//TileRenderer->CreateAnimationToFrame("YOU_TEXT", "Actor.bmp", { 865, 889, 913 }, 0.2f, true);
 				TileRenderer->ChangeAnimation("YOU_TEXT");
@@ -178,6 +188,7 @@ void PuzzleLevel::Start()
 			else if (MapTexture->GetColor(RGB(0, 0, 0), { fx, fy }) == RGB(200, 200, 0))
 			{
 				TileRenderer = TileGrid->SetTile(x, y, 864, BackGridPos);
+				TileRenderer->SetName("WIN_TEXT");
 				//TileRenderer->CreateAnimationToFrame("WIN_TEXT", "Actor.bmp", { 866, 890, 914 }, 0.2f, true);
 				TileRenderer->CreateAnimationToFrame("WIN_TEXT", "Actor.bmp", { 867, 891, 915 }, 0.2f, true);
 				TileRenderer->ChangeAnimation("WIN_TEXT");
@@ -187,91 +198,10 @@ void PuzzleLevel::Start()
 			else if (MapTexture->GetColor(RGB(0, 0, 0), { fx, fy }) == RGB(0, 100, 0))
 			{
 				TileRenderer = TileGrid->SetTile(x, y, 868, BackGridPos);
+				TileRenderer->SetName("STOP_TEXT");
 				TileRenderer->CreateAnimationToFrame("STOP_TEXT", "Actor.bmp", { 868, 892, 916 }, 0.2f, true);
 				//TileRenderer->CreateAnimationToFrame("STOP_TEXT", "Actor.bmp", { 869, 893, 917 }, 0.2f, true);
 				TileRenderer->ChangeAnimation("STOP_TEXT");
-			}
-		}
-	}
-
-	// ∆€¡Ò ¡§¿«
-	for (int y = 0; y < 15; y++)
-	{
-		for (int x = 0; x < 21; x++)
-		{
-			float fx = x;
-			float fy = y;
-
-			// WALL
-			if (MapTexture->GetColor(RGB(0, 0, 0), { fx, fy }) == RGB(255, 0, 0))
-			{
-				TileRenderer->SetName("WALL");
-			}
-
-			// GRASS
-			else if (MapTexture->GetColor(RGB(0, 0, 0), { fx, fy }) == RGB(0, 180, 0))
-			{
-				TileRenderer->SetName("GRASS");
-			}
-
-			// BABA
-			else if (MapTexture->GetColor(RGB(0, 0, 0), { fx, fy }) == RGB(0, 0, 0))
-			{
-				TileRenderer->SetName("BABA");
-			}
-
-			// BLOCK
-			else if (MapTexture->GetColor(RGB(0, 0, 0), { fx, fy }) == RGB(0, 0, 100))
-			{
-				TileRenderer->SetName("BLOCK");
-			}
-
-			// FLAG
-			else if (MapTexture->GetColor(RGB(0, 0, 0), { fx, fy }) == RGB(255, 255, 0))
-			{
-				TileRenderer->SetName("FLAG");
-			}
-
-			// WALL_TEXT
-			else if (MapTexture->GetColor(RGB(0, 0, 0), { fx, fy }) == RGB(50, 50, 50))
-			{
-				TileRenderer->SetName("WALL_TEXT");
-			}
-
-			// IS_TEXT
-			else if (MapTexture->GetColor(RGB(0, 0, 0), { fx, fy }) == RGB(200, 200, 200))
-			{
-				TileRenderer->SetName("IS_TEXT");
-			}
-
-			// FLAG_TEXT
-			else if (MapTexture->GetColor(RGB(0, 0, 0), { fx, fy }) == RGB(255, 200, 0))
-			{
-				TileRenderer->SetName("FLAG_TEXT");
-			}
-
-			// BABA_TEXT
-			else if (MapTexture->GetColor(RGB(0, 0, 0), { fx, fy }) == RGB(255, 100, 100))
-			{
-				TileRenderer->SetName("BABA_TEXT");
-			}
-
-			// YOU_TEXT
-			else if (MapTexture->GetColor(RGB(0, 0, 0), { fx, fy }) == RGB(100, 50, 50))
-			{
-				TileRenderer->SetName("YOU_TEXT");
-			}
-
-			// WIN_TEXT
-			else if (MapTexture->GetColor(RGB(0, 0, 0), { fx, fy }) == RGB(200, 200, 0))
-			{
-				TileRenderer->SetName("WIN_TEXT");
-			}
-
-			// STOP_TEXT
-			else if (MapTexture->GetColor(RGB(0, 0, 0), { fx, fy }) == RGB(0, 100, 0))
-			{
-				TileRenderer->SetName("STOP_TEXT");
 			}
 		}
 	}
